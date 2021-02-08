236ab08ee7c27000eeb86b011422a7e28d67c424
bucklandg <condellogregory@gmail.com>
2021-02-08 11:50:54 +0100

    .

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-  res.render('home', {msg: 'handlebars are Cool!'});
-})
+// app.get('/', (req, res) => {
+//   res.render('home', {msg: 'handlebars are Cool!'});
+// })
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
@@ -14,3 +14,16 @@ var exphbs = require('express-handlebars');
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+
+// OUR MOCK ARRAY OF PROJECTS
+let reviews = [
+  {title: "Great Review", movieTitle: "Batman II"},
+  {title: "Awesome Movie", movieTitle: "Titanic"},
+  {title: "Let's go Fuckers!", movieTitle: "Trailer Park Boys"}
+]
+
+// INDEX
+app.get('/', (req, res) => {
+  res.render('reviews-index', {reviews: reviews});
+})
